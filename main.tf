c0510c36b10396641854a2cfbfc25c6effe6999c
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2022-05-01 15:41:08 -0400

    github to pat

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_launch_template" "ec2_launch_template" {
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  user_data = base64encode(templatefile("${path.cwd}/bootstrap.sh", { github_repo_url = var.github_repo_url, github_repo_pat_token = var.github_repo_pat_token, runner_name = var.runner_name, labels = join(",", var.labels) }))
+  user_data = base64encode(templatefile("${path.cwd}/bootstrap.sh", { github_repo_url = var.github_repo_url, personal_access_token = var.personal_access_token, runner_name = var.runner_name, labels = join(",", var.labels) }))
   tags = {
     Name = "github_runner"
   }
